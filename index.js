644334b88d2f62716a2806edb1ab5337c4e11574
Gordon <gordonj2016@outlook.com>
2025-07-17 12:13:56 -0500

    fourth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 morgan.token('body', (request) => {return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :response-time ms - :body'))
+app.use(express.static('dist'))
 app.use(cors())
 
 let persons = [
